f5ea06eb4f3cb9010b7a27176403d4427be866fe
Jeanlee <jeanlee.barreto@utec.edu.pe>
2022-05-22 22:49:23 -0500

    Add final code for Lab5a

--- lab5a/solution.py
+++ lab5a/solution.py
@@ -1,74 +1,87 @@
+# GITHUB REPO: https://github.com/JeanleeRoy/CS3I01-seguridad-labs/blob/master/lab5a/lab5a.ipynb
+
 from cryptography.hazmat.primitives import hashes
-from tail_recursion import tail_recursive, recurse
-#import matplotlib.pyplot as plt
 import time
-import random
-import math
 
 def hash(msg: str):
 	digest = hashes.Hash(hashes.SHA1())
 	digest.update(msg.encode("unicode_escape"))
 	return digest.finalize().hex()
 
-example = "1:20:1303030600:jpazos@utec.edu.pe::McMybZIhxKXu57jd:ckvi"
-
 ## RECURSIVE HASHCASH
 
 def recursiveHashCash(ver: int, bits: int, resource: str, iv: str, ctr: int):
-	baseStr = makeBaseStr(ver, bits, getRTime(), resource, iv, ctr)
-	hashed = hash(baseStr)
-
-	if checkBits(hashed, bits):
-		return hashed
-	else:
-		return recursiveHashCash(ver, bits, resource, iv, ctr)
+    baseStr = makeBaseStr(ver, bits, getRTime(), resource, iv, ctr)
+    
+    if checkBits(baseStr, bits):
+        return baseStr
+    else:
+        return recursiveHashCash(ver, bits, resource, iv, ctr+1)
 
 def makeBaseStr(*args):
-	return ':'.join([str(s) for s in args])
+	return f"{args[0]}:{args[1]}:{args[2]}:{args[3]}::{args[4]}:{args[5]}"
 
-def checkBits(hashed: str, bits: int):
+def checkBits(baseString: str, bits: int):
+	hashed = hash(baseString)
 	bitCheck = bits//4
 	return hashed[:bitCheck] == "0"*bitCheck
 
 def getRTime():
-	return int(time.time())+int(random.random()*10**8)
+	return int(time.time())
 
 
 ## RECURSIVE HASHCASH
-# Apply tail recursion
+# tail_recursion: code taken from https://chrispenner.ca/posts/python-tail-recursion
+class Recurse(Exception):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
 
+def recurse(*args, **kwargs):
+    raise Recurse(*args, **kwargs)
+        
+def tail_recursive(f):
+    def decorated(*args, **kwargs):
+        while True:
+            try:
+                return f(*args, **kwargs)
+            except Recurse as r:
+                args = r.args
+                kwargs = r.kwargs
+                continue
+    return decorated
+
+# Apply tail recursion
 @tail_recursive
 def tailRecursiveHashCash(ver: int, bits: int, resource: str, iv: str, ctr: int):
-    curTime = str(int(time.time()) + int(random.random()*10**7))
-    baseStr = str(ver) + ':' + str(bits) + ':' + curTime + ':' + resource + ':' + iv + ':' + str(ctr)
-    hashed = hash(baseStr)
+    baseStr = makeBaseStr(ver, bits, getRTime(), resource, iv, ctr)
 
-    if checkBits(hashed, bits):
-        return hashed
-    recurse(ver, bits, resource, iv, ctr)
+    if checkBits(baseStr, bits):
+        return baseStr
+    recurse(ver, bits, resource, iv, ctr+1)
 
 
 ## ITERATIVE HASHCASH
 
 def iterativeHashCash(ver: int, bits: int, resource: str, iv: str, ctr: int):
-	while True:
-		curTime = getRTime()
-		baseStr = makeBaseStr(ver, bits, curTime, resource, iv, ctr)
-		hashed = hash(baseStr)
-		if checkBits(hashed, bits):
-			return hashed
+    while True:
+        baseStr = makeBaseStr(ver, bits, getRTime(), resource, iv, ctr)
+        ctr+=1
+        if checkBits(baseStr, bits):
+            return baseStr
 
 
 ## ITERATIVE HASHCASH PARTIAL
 
 def iterativeHashCashPartial(ver: int, bits: int, resource: str, iv: str, ctr: int):
-	while True:
-		baseStr = makeBaseStr(ver, bits, getRTime(), resource, iv, ctr)
-		hashed = hash(baseStr)
-		if checkBitsPartial(hashed, bits):
-			return hashed
+    while True:
+        baseStr = makeBaseStr(ver, bits, getRTime(), resource, iv, ctr)
+        ctr += 1
+        if checkBitsPartial(baseStr, bits):
+            return baseStr
 
-def checkBitsPartial(hashed: str, bits: int):
+def checkBitsPartial(baseString: str, bits: int):
+	hashed = hash(baseString)
 	bHashed = hextobin(hashed)
 	return bHashed[:bits] == "0"*bits
 
